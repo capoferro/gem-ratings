5a39dab2d07b8cb2ab03385b432c6e43b51bf129
Josiah Kiehl <josiah@bluepojo.com>
2011-02-13 20:47:43 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GemRatings::Application.config.secret_token = '36dc268f044c88bfe851b3586aee6bc1474f57589a88da5e16b38f3121ca1b24364fa81f3449fb603f6b30004644918b67e749c3e6452b0f12eb17188d4305cc'
